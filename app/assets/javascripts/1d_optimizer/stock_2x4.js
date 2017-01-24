b96318ff968c85719d98a204b39065993e29d9a1
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2017-01-24 11:26:32 +0000

    added JS method and tests

---
+++ app/assets/javascripts/1d_optimizer/stock_2x4.js
@@ -0,0 +1,6 @@
+var stock_2x4 = {
+  name: "2 by 4",
+  length: 2.4
+};
+
+module.exports = stock_2x4;
---
+++ test/specs/1d_optimizer/stock_2x4_spec.js
@@ -0,0 +1,16 @@
+var stock_2x4 = require('../../../app/assets/javascripts/1d_optimizer/stock_2x4');
+var assert = require('assert');
+
+// Item should have name
+describe('stock_2x4', function(){
+ it('should have name', 
+  function(){
+    assert.equal("2 by 4", stock_2x4.name);
+  });
+
+ // Item should have length
+ it('should have length', 
+  function(){
+    assert.equal(2.4, stock_2x4.length);
+  });
+});

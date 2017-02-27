22bb18cfcc17ceb181a2825f8516ac542e9e7a77
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2017-02-27 09:04:43 +0000

    added converter

--- config/routes.rb
+++ config/routes.rb
@@ -46,8 +46,12 @@ Rails.application.routes.draw do
 
   get '/ball' => 'pages#ball'
 
+# Week 5 -----------------------------------------------------------------------
+  resources :converter do 
+        
+    end
 
-
+  get '/converter' => 'pages#converter'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

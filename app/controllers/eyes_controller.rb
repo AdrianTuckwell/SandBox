bda0387b7fc9a92f65e5fdc036fb02000e609552
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2017-03-25 11:16:43 +0000

    Added week 8 eye app

---
+++ app/controllers/eyes_controller.rb
@@ -0,0 +1,4 @@
+class EyesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -67,6 +67,13 @@ Rails.application.routes.draw do
 
   get '/,born' => 'pages#born'
 
+# Week 8 -----------------------------------------------------------------------
+  resources :eyes do 
+        
+    end
+
+  get '/,eyes' => 'pages#eyes'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

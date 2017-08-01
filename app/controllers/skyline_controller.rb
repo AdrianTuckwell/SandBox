7534c063209adb6ca71f06594cb49c2788bc5b62
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2017-08-01 12:23:46 +0100

    Added skyline

---
+++ app/controllers/skyline_controller.rb
@@ -0,0 +1,4 @@
+class SkylineController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -192,6 +192,14 @@ Rails.application.routes.draw do
 
   get '/,stopwatch' => 'pages#stopwatch'
 
+# Week 26 -----------------------------------------------------------------------
+  resources :skyline do 
+        
+    end
+
+  get '/,skyline' => 'pages#skyline'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

f7ba52b5ede7779b7fbb886c462cd456a57473d0
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2017-08-31 12:17:24 +0100

    Added calculator

--- config/routes.rb
+++ config/routes.rb
@@ -213,6 +213,13 @@ Rails.application.routes.draw do
   get '/,calendar' => 'pages#calendar'
 
 
+# Week 29 -----------------------------------------------------------------------
+  resources :calculator do 
+        
+    end
+
+  get '/,calculator' => 'pages#calculator'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
